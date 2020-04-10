a8dd43d238d229c108937f51f8626e8989a62189
David Todd <me@david.to>
2020-04-10 15:19:00 +0100

    Updates to content

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,39 @@
 const express = require('express')
 const router = express.Router(null)
+var Airtable = require('airtable')
+var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
+	'appOl2BBiMCdby3zB'
+)
 
 router.all('*', (req, res, next) => {
-  req.session.data.loggedIn = req.user !== false && req.user !== undefined
-  req.session.data.errors = false
-  req.session.data.time = Date.now()
-  req.session.data.successFlash = false
-  req.session.data.fromCheck = false
-  req.session.data.fatalError = false
-  next()
+	req.session.data.loggedIn = req.user !== false && req.user !== undefined
+	req.session.data.errors = false
+	req.session.data.time = Date.now()
+	req.session.data.successFlash = false
+	req.session.data.fromCheck = false
+	req.session.data.fatalError = false
+	next()
+})
+
+router.post('/support-handler', (req, res) => {
+	base('Website responses').create(
+		[
+			{
+				fields: {
+					'Email address': req.body['_email'],
+					Message: req.body['_message'],
+				},
+			},
+		],
+		function (err, records) {
+			if (err) {
+				console.error(err)
+				res.redirect('/support')
+				return
+			}
+			res.redirect('/support-success')
+		}
+	)
 })
 
 module.exports = router
---
+++ public/javascripts/app.js
@@ -0,0 +1,69 @@
+"use strict";
+
+var docBody = document.querySelector('body');
+var narrowMenuToggleButtons = document.querySelectorAll('a[aria-controls="narrow-menu-container"]');
+
+var toggleNarrowMenu = function toggleNarrowMenu(e) {
+  e.preventDefault();
+  e.stopPropagation();
+  var narrowMenuContainer = document.querySelector('#narrow-menu-container');
+  var narrowMenuIsOpen = narrowMenuContainer.getAttribute('aria-expanded') === 'true';
+
+  if (narrowMenuIsOpen) {
+    narrowMenuToggleButtons.forEach(function (button) {
+      return button.classList.remove('opened');
+    });
+    narrowMenuContainer.setAttribute('aria-expanded', 'false');
+  } else {
+    narrowMenuToggleButtons.forEach(function (button) {
+      return button.classList.add('opened');
+    });
+    narrowMenuContainer.setAttribute('aria-expanded', 'true');
+  }
+};
+
+var setHeaderHeight = function setHeaderHeight() {
+  var headerHeight = document.querySelector('header#site-header').offsetHeight;
+  docBody.style.setProperty('--headerHeight', "".concat(headerHeight, "px"));
+};
+
+var setPageScrollingStatus = function setPageScrollingStatus() {
+  if (window.scrollY > 1) {
+    document.body.classList.add('scrolling');
+  } else {
+    document.body.classList.remove('scrolling');
+  }
+};
+
+setHeaderHeight();
+setPageScrollingStatus();
+window.addEventListener('resize', setHeaderHeight);
+window.addEventListener('scroll', setPageScrollingStatus);
+narrowMenuToggleButtons.forEach(function (button) {
+  return button.addEventListener('click', toggleNarrowMenu);
+});
+"use strict";
+
+/* global $ */
+$('body').on('submit', 'form', function () {
+  // On form submit, add hidden inputs for checkboxes so the server knows if
+  // they've been unchecked. This means we can automatically store and update
+  // all form data on the server, including checkboxes that are checked, then
+  // later unchecked
+  var $checkboxes = $(this).find('input:checkbox');
+  var $inputs = [];
+  var names = {};
+  $checkboxes.each(function () {
+    var $this = $(this);
+
+    if (!names[$this.attr('name')]) {
+      names[$this.attr('name')] = true;
+      var $input = $('<input type="hidden">');
+      $input.attr('name', $this.attr('name'));
+      $input.attr('value', '_unchecked');
+      $inputs.push($input);
+    }
+  });
+  $(this).prepend($inputs);
+});
+//# sourceMappingURL=app.js.map
