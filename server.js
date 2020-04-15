aab317827c0800917bdff81617ceff4839ba1582
David Todd <me@david.to>
2020-04-15 11:29:23 +0100

    removed robots text

--- server.js
+++ server.js
@@ -39,26 +39,26 @@ useHttps = useHttps.toLowerCase()
 // asking for username/password twice (for `http`, then `https`).
 const isSecure = env === 'production' && useHttps === 'true'
 if (isSecure) {
-  app.use(utils.forceHttps)
-  app.set('trust proxy', 1) // needed for secure cookies on heroku
+	app.use(utils.forceHttps)
+	app.set('trust proxy', 1) // needed for secure cookies on heroku
 }
 
-middleware.forEach(func => app.use(func))
+middleware.forEach((func) => app.use(func))
 
 // Set up App
 let appViews = extensions.getAppViews([
-  path.join(__dirname, '/app/views/'),
-  path.join(__dirname, '/lib/')
+	path.join(__dirname, '/app/views/'),
+	path.join(__dirname, '/lib/'),
 ])
 
 let nunjucksConfig = {
-  autoescape: true,
-  noCache: true,
-  watch: false // We are now setting this to `false` (it's by default false anyway) as having it set to `true` for production was making the tests hang
+	autoescape: true,
+	noCache: true,
+	watch: false, // We are now setting this to `false` (it's by default false anyway) as having it set to `true` for production was making the tests hang
 }
 
 if (env === 'development') {
-  nunjucksConfig.watch = true
+	nunjucksConfig.watch = true
 }
 
 nunjucksConfig.express = app
@@ -77,18 +77,18 @@ app.use('/public', express.static(path.join(__dirname, '/public')))
 // Support for parsing data in POSTs
 app.use(bodyParser.json())
 app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+	bodyParser.urlencoded({
+		extended: true,
+	})
 )
 
 // Add global variable to determine if DoNotTrack is enabled.
 // This indicates a user has explicitly opted-out of tracking.
 // We can avoid injecting third-party scripts that do not respect this decision.
 app.use(function (req, res, next) {
-  // See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/DNT
-  res.locals.doNotTrackEnabled = req.header('DNT') === '1'
-  next()
+	// See https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/DNT
+	res.locals.doNotTrackEnabled = req.header('DNT') === '1'
+	next()
 })
 
 // Add variables that are available in all views
@@ -103,95 +103,89 @@ app.locals.extensionConfig = extensions.getAppConfig()
 // Session uses service name to avoid clashes with other prototypes
 const sessionName = 'shc-portal'
 let sessionOptions = {
-  secret: sessionName,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 4, // 4 hours
-    secure: isSecure
-  }
+	secret: sessionName,
+	cookie: {
+		maxAge: 1000 * 60 * 60 * 4, // 4 hours
+		secure: isSecure,
+	},
 }
 
 // Support session data in cookie or memory
 if (useCookieSessionStore === 'true') {
-  app.use(
-    sessionInCookie(
-      Object.assign(sessionOptions, {
-        cookieName: sessionName,
-        proxy: true,
-        requestKey: 'session'
-      })
-    )
-  )
+	app.use(
+		sessionInCookie(
+			Object.assign(sessionOptions, {
+				cookieName: sessionName,
+				proxy: true,
+				requestKey: 'session',
+			})
+		)
+	)
 } else {
-  app.use(
-    sessionInMemory(
-      Object.assign(sessionOptions, {
-        name: sessionName,
-        resave: false,
-        saveUninitialized: false
-      })
-    )
-  )
+	app.use(
+		sessionInMemory(
+			Object.assign(sessionOptions, {
+				name: sessionName,
+				resave: false,
+				saveUninitialized: false,
+			})
+		)
+	)
 }
 
 // Automatically store all data users enter
 if (useAutoStoreData === 'true') {
-  app.use(utils.autoStoreData)
-  utils.addCheckedFunction(nunjucksAppEnv)
+	app.use(utils.autoStoreData)
+	utils.addCheckedFunction(nunjucksAppEnv)
 }
 
 // Prevent search indexing
 app.use(function (req, res, next) {
-  // Setting headers stops pages from being indexed even if indexed pages link to them.
-  res.setHeader('X-Robots-Tag', 'noindex')
-  next()
-})
-
-app.get('/robots.txt', function (req, res) {
-  res.type('text/plain')
-  res.send('User-agent: *\nDisallow: /')
+	// Setting headers stops pages from being indexed even if indexed pages link to them.
+	next()
 })
 
 // Load routes (found in app/routes.js)
 if (typeof routes !== 'function') {
-  console.log(routes.bind)
-  routes.bind(app)
+	console.log(routes.bind)
+	routes.bind(app)
 } else {
-  app.use('/', routes)
+	app.use('/', routes)
 }
 
 // Strip .html and .htm if provided
 app.get(/\.html?$/i, function (req, res) {
-  var path = req.path
-  var parts = path.split('.')
-  parts.pop()
-  path = parts.join('.')
-  res.redirect(path)
+	var path = req.path
+	var parts = path.split('.')
+	parts.pop()
+	path = parts.join('.')
+	res.redirect(path)
 })
 
 // Auto render any view that exists
 
 // App folder routes get priority
 app.get(/^([^.]+)$/, function (req, res, next) {
-  utils.matchRoutes(req, res, next)
+	utils.matchRoutes(req, res, next)
 })
 
 // Redirect all POSTs to GETs - this allows users to use POST for autoStoreData
 app.post(/^\/([^.]+)$/, function (req, res) {
-  res.redirect('/' + req.params[0])
+	res.redirect('/' + req.params[0])
 })
 
 // Catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error(`Page not found: ${req.path}`)
-  err.status = 404
-  next(err)
+	var err = new Error(`Page not found: ${req.path}`)
+	err.status = 404
+	next(err)
 })
 
 // Display error
 app.use(function (err, req, res) {
-  console.error(err.message)
-  res.status(err.status || 500)
-  res.send(err.message)
+	console.error(err.message)
+	res.status(err.status || 500)
+	res.send(err.message)
 })
 
 module.exports = app
