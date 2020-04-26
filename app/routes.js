1463d17aca8c1952d6636a4bd77dbfefadeeae79
David Todd <me@david.to>
2020-04-26 16:19:40 +0100

    Updates to receipt validation

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router(null)
 var Airtable = require('airtable')
+const https = require('https')
 var base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
 	'appOl2BBiMCdby3zB'
 )
@@ -34,6 +35,44 @@ router.post('/support-handler', (req, res) => {
 			res.redirect('/support-success')
 		}
 	)
+})
+
+// App store receipt verification
+
+router.post('/verifyAppStoreReceipt', (req, res) => {
+	let receiptData = req.body['_receipt-data']
+
+	const data = JSON.stringify({
+		'receipt-data': receiptData,
+		password: '0e8bfb00b7294888bf17671398922470',
+		'exclude-old-transactions': false,
+	})
+
+	const options = {
+		hostname: 'sandbox.itunes.apple.com',
+		port: 443,
+		path: '/verifyReceipt',
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Content-Length': data.length,
+		},
+	}
+
+	const request = https.request(options, (response) => {
+		console.log(`statusCode: ${response.statusCode}`)
+
+		response.on('data', (d) => {
+			res.send(d)
+		})
+	})
+
+	request.on('error', (error) => {
+		console.error(error)
+	})
+
+	request.write(data)
+	request.end()
 })
 
 module.exports = router
